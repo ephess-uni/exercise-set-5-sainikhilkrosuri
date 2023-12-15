12c79ac5e70ee0e76a83c540ddae6c8a8396bed2
Sai Nikhil Krosuri <110455764+sainikhilkrosuri@users.noreply.github.com>
2023-12-15 11:11:11 -0600

    Update ex_5_2.py

--- src/ex_5_2.py
+++ src/ex_5_2.py
@@ -21,10 +21,8 @@ if __name__ == "__main__":
     root_dir = get_repository_root()
     INFILE = root_dir / "data" / "ex_5_2-data.csv"
     OUTFILE = root_dir / "outputs" / "ex_5_2-processed.csv"
-
-    data_main = np.loadtxt(INFILE, delimiter=',')
-    zero_mean_data = data - np.mean(data_main, axis=0)
-
-    scaled_data = zero_mean_data / np.std(zero_mean_data, axis=0)
-
-    np.savetxt(OUTFILE, scaled_data, delimiter=',')
+    data = np.loadtxt(INFILE)
+    mean = np.mean(data)
+    std = np.std(data)
+    processed = (data - mean) / std
+    np.savetxt(OUTFILE, processed)
